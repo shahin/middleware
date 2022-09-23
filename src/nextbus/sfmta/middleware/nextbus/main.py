890678b6ec8b16edece0aae6bbc3eb97ae035347
shahin <shahin@users.noreply.github.com>
2022-09-23 15:25:20 -0700

    Build 9/21 comparison report from umoiq only

--- src/nextbus/sfmta/middleware/nextbus/main.py
+++ src/nextbus/sfmta/middleware/nextbus/main.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import json
 import os
 import sys
 import time
@@ -26,7 +27,9 @@ fields = {
     'direction_id': lambda preds, pred: pred['direction']['id'],
     'direction_name': lambda preds, pred: pred['direction']['name'],
     'trip_id': lambda preds, pred: pred['tripId'],
+    'min_vehicle_id': lambda preds, pred: min(pred['linkedVehicleIds'].split(',')),
     'vehicle_id': lambda preds, pred: pred['vehicleId'],
+    'linked_vehicle_ids': lambda preds, pred: pred['linkedVehicleIds'],
 }
 
 
@@ -44,22 +47,38 @@ def parse_predictions(response_data: dict[str, Any]) -> list[dict[str, str]]:
 @argh.arg('stop_code', help='The stop code to request predictions for, e.g. 15419')
 @argh.arg('--every-n-seconds', '-e', help='Request predictions every N seconds')
 @argh.arg('--agency', help='The UmoIQ agency identifier to request predictions for')
-def main(stop_code: str, every_n_seconds: int = 60.0, agency: str = 'sf-muni'):
+@argh.arg('--response-data-path', help='Name of the file to write raw response data to')
+@argh.arg('--start-at', help='Time to start at, in YYYYmmddTHHMMSS format')
+def main(stop_code: str, every_n_seconds: int = 60.0, agency: str = 'sf-muni', response_data_path: str = None, start_at: str = None):
     umoiq_url = UMOIQ_URL_TMPL.format(stop_code=stop_code, umoiq_api_key=UMOIQ_API_KEY, agency=agency)
 
     writer = csv.writer(sys.stdout, dialect='excel')
     writer.writerow([header.replace('_', ' ') for header in fields.keys()])
 
-    starttime = time.time()
+    if response_data_path is None:
+        response_data_path = f"responses_{datetime.now().strftime('%Y%m%dT%H%M%S')}_{agency}.json"
+    raw_f = open(response_data_path, 'w')
+
+    if start_at is not None:
+        start_at_date = datetime.strptime(start_at, "%Y%m%dT%H%M%S")
+
+    now = datetime.now()
+    time.sleep((start_at_date-now).total_seconds())
+
     while True:
         resp = requests.get(umoiq_url)
-        predictions = parse_predictions(resp.json())
+
+        response_data = resp.json()
+        raw_f.write(json.dumps(response_data))
+        raw_f.flush()
+
+        predictions = parse_predictions(response_data)
         
         for prediction in predictions:
             writer.writerow(prediction.values())
         sys.stdout.flush()
 
-        time.sleep(every_n_seconds - ((time.time() - starttime) % every_n_seconds))
+        time.sleep(every_n_seconds - ((datetime.now() - start_at_date).total_seconds() % every_n_seconds))
 
 
 if __name__ == '__main__':
